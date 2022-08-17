0c2d5d14ca0f36c3e63803b6b4ce6b90597b0e29
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-17 07:57:26 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        array = arr.filter do |letters|
+            @word.chars.sort == letters.chars.sort
+        end
+        array
+    end
+end
